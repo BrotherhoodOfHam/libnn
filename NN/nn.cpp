fa05aea3091b30274a3f27bb130a4c9d3cf8a14a
BrotherhoodOfHam <thomas.street@florimont.com>
2020-02-08 19:40:39 +0000

    Some reorganization, separated layer from activation function.

--- NN/nn.cpp
+++ NN/nn.cpp
@@ -61,7 +61,7 @@ public:
 	};
 
 	matrix(size_t _rows, size_t _cols) :
-		m(_rows* _cols),
+		m(_rows * _cols),
 		rows(_rows),
 		cols(_cols)
 	{}
@@ -108,53 +108,156 @@ struct layer
 	{}
 };
 
-class nn_layer
+
+class nn_node
 {
-	matrix w;
-	vector b;
-	activation activation_type;
-	bool is_output;
+private:
+
+	size_t _input_size;
+	size_t _output_size;
 
 public:
 
-	struct actvn
+	nn_node(size_t input_size, size_t output_size) :
+		_input_size(input_size), _output_size(output_size)
+	{}
+
+	nn_node(const nn_node&) = delete;
+
+	size_t input_size() const { return _input_size; }
+	size_t output_size() const { return _output_size; }
+
+	virtual void forward(const vector& x, vector& y) const = 0;
+
+	virtual void backward(const vector& y, const vector& x, const vector& dy, vector& dx, matrix& dw, vector& db) const = 0;
+
+	virtual void update_params(const matrix& dw, const vector& db, float r, float k) = 0;
+};
+
+class nn_activation : public nn_node
+{
+	activation _type;
+	bool _isoutput;
+
+public:
+
+	nn_activation(size_t input_size, activation type, bool is_output) :
+		_type(type), _isoutput(is_output),
+		nn_node(input_size, input_size)
+	{}
+
+	void forward(const vector& x, vector& y) const override
 	{
-		vector a;
-		vector z;
+		assert(x.length == y.length);
 
-		actvn(const actvn&) = delete;
+		switch (_type)
+		{
+			case activation::linear:
+			{
+				for (size_t i = 0; i < x.length; i++)
+					y[i] = x[i];
+				break;
+			}
+			case activation::relu:
+			{
+				for (size_t i = 0; i < x.length; i++)
+					y[i] = std::max(x[i], 0.0f);
+				break;
+			}
+			case activation::sigmoid:
+			{
+				for (size_t i = 0; i < x.length; i++)
+					y[i] = 1.0f / (1.0f + std::exp(-x[i]));
+				break;
+			}
+			case activation::softmax:
+			{
+				scalar sum = 0.0f;
+				for (size_t i = 0; i < x.length; i++)
+				{
+					y[i] = std::exp(x[i]);
+					sum += y[i];
+				}
+				for (size_t i = 0; i < x.length; i++)
+					y[i] /= sum;
+				break;
+			}
+		}
+	}
 
-		actvn(vector _a, vector _z) :
-			a(std::move(_a)), z(std::move(_z))
-		{}
-
-		actvn(actvn&& rhs) noexcept :
-			a(std::move(rhs.a)), z(std::move(rhs.z))
-		{}
-	};
-
-	struct delta
+	void backward(const vector& y, const vector& x, const vector& dy, vector& dx, matrix& dw, vector& db) const override
 	{
-		matrix dw;
-		vector db;
-		vector dx;
+		assert(x.length == y.length);
+		assert(x.length == dy.length);
+		assert(x.length == dx.length);
 
-		delta(const delta&) = delete;
+		//workaround for when this is the last layer
+		if (_isoutput)
+		{
+			for (size_t i = 0; i < x.length; i++)
+				dx[i] = dy[i];
+			return;
+		}
 
-		delta(matrix _dw, vector _db, vector _dx) :
-			dw(std::move(_dw)), db(std::move(_db)), dx(std::move(_dx))
-		{}
+		switch (_type)
+		{
+			case activation::linear:
+			{
+				for (size_t i = 0; i < x.length; i++)
+					dx[i] = dy[i];
+				break;
+			}
+			case activation::relu:
+			{
+				for (size_t i = 0; i < x.length; i++)
+					if (x[i] > 0.0f)
+						dx[i] = dy[i];
+					else
+						dx[i] = 0.0f;
+				break;
+			}
+			case activation::sigmoid:
+			{
+				for (size_t i = 0; i < x.length; i++)
+				{
+					dx[i] = (y[i] * (1.0f - y[i])) * dy[i];
+				}
+				break;
+			}
+			case activation::softmax:
+			{
+				for (size_t j = 0; j < x.length; j++)
+				{
+					float sum = 0.0f;
+					for (size_t i = 0; i < x.length; i++)
+					{
+						float dydz = (i == j)
+							? y[i] * (1 - y[i])
+							: -y[i] * y[j];
 
-		delta(delta&& rhs) noexcept :
-			dw(std::move(rhs.dw)), db(std::move(rhs.db)), dx(std::move(rhs.dx))
-		{}
-	};
+						sum += dy[i] * dydz;
+					}
+					dx[j] = sum;
+				}
+				break;
+			}
+		}
+	}
 
-	nn_layer(size_t layer_size, size_t input_size, activation _activation_type, bool _is_output) :
+	void update_params(const matrix& dw, const vector& db, float k, float r) override { }
+};
+
+class nn_layer : public nn_node
+{
+	matrix w;
+	vector b;
+
+public:
+
+	nn_layer(size_t input_size, size_t layer_size) :
 		w(layer_size, input_size),
 		b(layer_size),
-		activation_type(_activation_type),
-		is_output(_is_output)
+		nn_node(input_size, layer_size)
 	{
 		std::default_random_engine gen;
 		std::normal_distribution<float> dist(0, 1);
@@ -168,20 +271,13 @@ public:
 			b[i] = 0.0f;
 	}
 
-	inline size_t input_size() const { return w.cols; }
-	inline size_t layer_size() const { return w.rows; }
-
-	actvn alloc_activation() const { return actvn(vector(layer_size()), vector(layer_size())); }
-	delta alloc_delta() const { return delta(matrix(w.rows, w.cols), vector(b.length), vector(input_size())); }
-
-	void forward(actvn& out, const vector& x) const
+	void forward(const vector& x, vector& y) const override
 	{
-		assert(out.a.length == w.rows);
-		assert(out.z.length == w.rows);
+		assert(y.length == w.rows);
 		assert(x.length == w.cols);
 
-		//for each activation a:
-		//a = f(w.x + b)
+		//for each row:
+		//y = w.x + b
 		for (size_t j = 0; j < w.rows; j++)
 		{
 			//z = w.x + b
@@ -189,196 +285,102 @@ public:
 			for (size_t i = 0; i < w.cols; i++)
 				z += x[i] * w[j][i];
 			z += b[j];
-
-			//cache z and a
-			out.z[j] = z;
+			y[j] = z;
 		}
-
-		activate(out.z, out.a);
 	}
 
-	void backward(delta& out, const vector& dy, const vector& x, const vector& z, const vector& y) const
+	void backward(const vector& y, const vector& x, const vector& dy, vector& dx, matrix& dw, vector& db) const override
 	{
-		assert(out.dw.rows == w.rows && out.dw.cols == w.cols);
-		assert(out.db.length == b.length);
-		assert(out.dx.length == input_size());
-		assert(dy.length == layer_size());
+		assert(dw.rows == w.rows && dw.cols == w.cols);
+		assert(db.length == b.length);
+		assert(dx.length == input_size());
+		assert(dy.length == output_size());
 		assert(x.length == input_size());
-		assert(z.length == layer_size());
+		assert(y.length == output_size());
 
-		// δ/δy = partial derivative of cost w.r.t to output activations (δC/δy)
+		// δ/δy = partial derivative of loss w.r.t to output
 		// the goal is to find the derivatives w.r.t to parameters: δw, δb, δx
 
-		if (is_output)
-		{
-			// in the case of the output layer we will assume: δb = δy
-			for (size_t i = 0; i < dy.length; i++)
-				out.db[i] = dy[i];
-		}
-		else
-		{
-			// store δz in δb
-			derivative(z, y, dy, out.db);
-		}
-		
 		// compute partial derivatives w.r.t weights and biases
 		for (size_t j = 0; j < w.rows; j++)
 		{
-			// δw = δz * x 
+			// δb = δy
+			db[j] = dy[j];
+			// δw = δy * x 
 			for (size_t i = 0; i < x.length; i++)
-				out.dw[j][i] = out.db[j] * x[i];
+				dw[j][i] = x[i] * dy[j];
 		}
 
 		// compute partial derivative w.r.t input x
 		for (size_t i = 0; i < w.cols; i++)
 		{
-			// δx = w^T * δz 
+			// δx = w^T * δy
 			scalar s = 0.0f;
 			for (size_t j = 0; j < w.rows; j++)
-				s += out.db[j] * w[j][i];
-			out.dx[i] = s;
+				s += w[j][i] * dy[j];
+			dx[i] = s;
 		}
 	}
 
-	void update_params(const delta& d, float k, float r)
+	void update_params(const matrix& dw, const vector& db, float k, float r) override
 	{
-		assert(d.dw.rows == w.rows && d.dw.cols == w.cols);
-		assert(d.db.length == b.length);
+		assert(dw.rows == w.rows && dw.cols == w.cols);
+		assert(db.length == b.length);
 
-		//apply gradient descent
+		// apply gradient descent
 		for (size_t j = 0; j < w.rows; j++)
 		{
-			b[j] -= k * d.db[j];
+			b[j] -= k * db[j];
 
 			for (size_t i = 0; i < w.cols; i++)
-				w[j][i] = (r * w[j][i]) - (k * d.dw[j][i]);
-		}
-	}
-
-private:
-
-	// compute activation
-	// a = f(z)
-	void activate(const vector& z, vector& a) const
-	{
-		assert(z.length == a.length);
-
-		switch (activation_type)
-		{
-			case activation::linear:
-			{
-				for (size_t i = 0; i < z.length; i++)
-					a[i] = z[i];
-				break;
-			}
-			case activation::relu:
-			{
-				for (size_t i = 0; i < z.length; i++)
-					a[i] = std::max(z[i], 0.0f);
-				break;
-			}
-			case activation::sigmoid:
-			{
-				for (size_t i = 0; i < z.length; i++)
-					a[i] = 1.0f / (1.0f + std::exp(-z[i]));
-				break;
-			}
-			case activation::softmax:
-			{
-				scalar sum = 0.0f;
-				for (size_t i = 0; i < z.length; i++)
-				{
-					a[i] = std::exp(z[i]);
-					sum += a[i];
-				}
-				for (size_t i = 0; i < z.length; i++)
-					a[i] /= sum;
-				break;
-			}
-		}
-	}
-
-	// compute partial derivative w.r.t to z
-	// δ/δz = δy/δz * δ/δy
-	void derivative(const vector& z, const vector& y, const vector& dy, vector& dz) const
-	{
-		assert(z.length == y.length);
-		assert(z.length == dy.length);
-		assert(z.length == dz.length);
-
-		switch (activation_type)
-		{
-			case activation::linear:
-			{
-				for (size_t i = 0; i < z.length; i++)
-					dz[i] = dy[i];
-				break;
-			}
-			case activation::relu:
-			{
-				for (size_t i = 0; i < z.length; i++)
-					if (z[i] > 0.0f)
-						dz[i] = dy[i];
-					else
-						dz[i] = 0.0f;
-				break;
-			}
-			case activation::sigmoid:
-			{
-				for (size_t i = 0; i < z.length; i++)
-				{
-					dz[i] = (y[i] * (1.0f - y[i])) * dy[i];
-				}
-				break;
-			}
-			case activation::softmax:
-			{
-				for (size_t j = 0; j < z.length; j++)
-				{
-					float sum = 0.0f;
-					for (size_t i = 0; i < z.length; i++)
-					{
-						float dydz = (i == j)
-							? y[i] * (1 - y[i])
-							: -y[i] * y[j];
-
-						sum += dy[i] * dydz;
-					}
-					dz[j] = sum;
-				}
-				break;
-			}
+				w[j][i] = (r * w[j][i]) - (k * dw[j][i]);
 		}
 	}
 };
 
+/*************************************************************************************************************************************/
+
 class nn
 {
-	std::vector<nn_layer> _layers;
-	std::vector<nn_layer::delta> _deltas;
-	std::vector<nn_layer::actvn> _actvns;
+	std::vector<std::unique_ptr<nn_node>> _layers;
+	std::vector<vector> _a;  // layer activations
+	std::vector<vector> _dy; // layer gradient
+	std::vector<matrix> _dw; // layer weight gradient
+	std::vector<vector> _db; // layer bias gradient
 
 public:
 
 	nn(size_t input_size, std::vector<layer> layers)
 	{
-		_layers.reserve(layers.size());
-		_deltas.reserve(layers.size());
-		_actvns.reserve(layers.size());
+		_layers.reserve(layers.size() * 2);
+		_a.reserve(_layers.size() + 1);
+		_dy.reserve(_layers.size() + 1);
+		_dw.reserve(_layers.size());
+		_db.reserve(_layers.size());
 
-		_actvns.push_back(nn_layer::actvn(vector(input_size), vector(1)));
+		_a.emplace_back(input_size);
+		_dy.emplace_back(input_size);
 
 		size_t i = 0;
 		size_t layer_input = input_size;
 
-		for (const auto& layer : layers)
+		for (const auto& layer_info : layers)
 		{
 			i++;
-			_layers.emplace_back(layer.size, layer_input, layer.actv, layers.size() == i);
-			_actvns.push_back(_layers.back().alloc_activation());
-			_deltas.push_back(_layers.back().alloc_delta());
+			
+			_layers.push_back(std::make_unique<nn_layer>(layer_input, layer_info.size));
+			_a.emplace_back(layer_info.size);
+			_dy.emplace_back(layer_info.size);
+			_dw.emplace_back(layer_info.size, layer_input);
+			_db.emplace_back(layer_info.size);
 
-			layer_input = layer.size;
+			_layers.push_back(std::make_unique<nn_activation>(layer_info.size, layer_info.actv, i == layers.size()));
+			_a.emplace_back(layer_info.size);
+			_dy.emplace_back(layer_info.size);
+			_dw.emplace_back(0, 0);
+			_db.emplace_back(0);
+
+			layer_input = layer_info.size;
 		}
 	}
 
@@ -391,7 +393,7 @@ public:
 
 		for (size_t ep = 0; ep < epochs; ep++)
 		{
-			std::cout << "epoch " << ep << ":\n" << std::endl;
+			std::cout << "epoch " << ep << ":" << std::endl;
 
 			std::shuffle(training_set.begin(), training_set.end(), std::default_random_engine(ep));
 
@@ -408,31 +410,73 @@ public:
 					last = t;
 				}
 
-				const auto& deltas = backprop(p.first, p.second);
-				for (size_t i = 0; i < _layers.size(); i++)
-				{
-					_layers[i].update_params(deltas[i], k, r);
-				}
+				train_batch(p.first, p.second, k, r);
 
 				c++;
 			}
 
-			double mse = 0.0;
+			double error = 0.0;
 			size_t correct = 0;
 			for (const auto& p : testing_set)
 			{
-				const auto& prediction = forward(p.first).back().a;
+				const auto& prediction = forward(p.first);
 
-				float mse = 0.0f;
 				for (size_t i = 0; i < prediction.length; i++)
-					mse += std::pow(prediction[i] - p.second[i], 2);
+					error += std::pow(prediction[i] - p.second[i], 2);
 				
 				if (arg_max(prediction) == arg_max(p.second))
 					correct++;
 			}
-			mse /= (2.0 * testing_set.size());
+			error /= (2.0 * testing_set.size());
 
-			std::cout << "mse: " << mse << " | recognised: (" << correct << " / " << testing_set.size() << ")" << std::endl;
+			std::cout << "mse: " << error << " | recognised: (" << correct << " / " << testing_set.size() << ")" << std::endl;
+		}
+	}
+
+	void train_batch(const vector& x, const vector& y, float k, float r)
+	{
+		assert(_layers.back()->output_size() == y.length);
+		assert(_layers.front()->input_size() == x.length);
+
+		//forward prop
+		for (size_t i = 0; i < x.length; i++)
+			_a[0][i] = x[i];
+
+		for (size_t i = 0; i < _layers.size(); i++)
+			_layers[i]->forward(_a[i], _a[i + 1]);
+
+		//backward prop
+		loss_derivative(_a.back(), y, _dy.back());
+
+		for (int i = _layers.size() - 1; i >= 0; i--)
+			_layers[i]->backward(_a[i + 1], _a[i], _dy[i + 1], _dy[i], _dw[i], _db[i]);
+
+		//gradient descent optimization
+		for (size_t i = 0; i < _layers.size(); i++)
+			_layers[i]->update_params(_dw[i], _db[i], k, r);
+	}
+
+	const vector& forward(const vector& x)
+	{
+		for (size_t i = 0; i < x.length; i++)
+			_a[0][i] = x[i];
+
+		for (size_t i = 0; i < _layers.size(); i++)
+			_layers[i]->forward(_a[i], _a[i + 1]);
+
+		return _a.back();
+	}
+
+private:
+
+	void loss_derivative(const vector& y, const vector& t, vector& dy)
+	{
+		assert(dy.length == t.length);
+		assert(dy.length == y.length);
+
+		for (size_t i = 0; i < dy.length; i++)
+		{
+			dy[i] = y[i] - t[i];
 		}
 	}
 
@@ -452,60 +496,6 @@ public:
 			}
 		}
 		return i_max;
-	}
-
-	const std::vector<nn_layer::actvn>& forward(const vector& x)
-	{
-		assert(x.length == _layers.front().input_size());
-		assert(x.length == _actvns[0].a.length);
-
-		_actvns[0].a = x;
-
-		for (size_t i = 0; i < _layers.size(); i++)
-		{
-			_layers[i].forward(_actvns[i+1], _actvns[i].a);
-		}
-
-		return _actvns;
-	}
-
-private:
-
-	const std::vector<nn_layer::delta>& backprop(const vector& x, const vector& y)
-	{
-		assert(_actvns.size() == _layers.size() + 1);
-		assert(_deltas.size() == _layers.size());
-		assert(_layers.back().layer_size() == y.length);
-		assert(_layers.front().input_size() == x.length);
-
-		const auto& actvns = forward(x);
-
-		assert(actvns.front().a.length == x.length);
-		assert(actvns.back().a.length == y.length);
-		
-		vector loss(_layers.back().layer_size());
-		loss_derivative(actvns.back().a, y, loss);
-
-		auto dy = std::ref(loss);
-
-		for (int i = _layers.size() - 1; i >= 0; i--)
-		{
-			_layers[i].backward(_deltas[i], dy, actvns[i].a, actvns[i + 1].z, actvns[i + 1].a);
-			dy = std::ref(_deltas[i].dx);
-		}
-
-		return _deltas;
-	}
-
-	void loss_derivative(const vector& y, const vector& t, vector& dy)
-	{
-		assert(dy.length == t.length);
-		assert(dy.length == y.length);
-
-		for (size_t i = 0; i < dy.length; i++)
-		{
-			dy[i] = y[i] - t[i];
-		}
 	}
 };
 
